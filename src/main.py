d3efe154e462a47dcb188bb01e0c74e316315b23
Xewus <xewuss@yandex.ru>
2023-02-25 17:05:32 +0300

    startup event

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
 from src.config import AVATARS_DIR, MEDIA_DIR, settings
 from src.users.router import router as users_router
 
-MEDIA_DIR.mkdir(exist_ok=True)
-AVATARS_DIR.mkdir(exist_ok=True)
+# MEDIA_DIR.mkdir(exist_ok=True)
+# AVATARS_DIR.mkdir(exist_ok=True)
 
 app = FastAPI(
     debug=settings.debug,
     title=settings.app_title,
     contact={'xewus': 'xewus@ya.ru'}
 )
-app.mount(str(MEDIA_DIR), StaticFiles(directory=MEDIA_DIR), 'media')
+
 app.include_router(users_router, tags=('Users',))
+
+
+@app.on_event('startup')
+async def create_dirs():
+    MEDIA_DIR.mkdir(exist_ok=True)
+    AVATARS_DIR.mkdir(exist_ok=True)
